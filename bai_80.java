f2fe21094b2e845302842e4b07c2e38b32a6465c
tienhq <tienhq@ciaosoft.vn>
2026-03-19 10:28:58 +0700

    Update bai_80.java

--- bai_80.java
+++ bai_80.java
@@ -15,4 +15,21 @@ public class bai_80{
         }
         System.out.printf("Kết quả S(n) = %.5f%n", s); 
     }
+//bài 71: Tính S(x,n) = -x + X^2/(1+2) - x^3/(1+2+3) + ... + (-1)^n*x^n/(1+2+...+n)
+//nk = (((-1)^k)*(2*x^k))/(k*(k+1))
+    public static void newmain(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập x: ");
+        double x = sc.nextDouble();
+        System.out.print("Nhập n: ");
+        int n = sc.nextInt();
+        double s = 0;
+        int dau = 1;
+        for (int k = 1; k <= n; k++){
+            double ms = (double) k * (k + 1)/2;
+            s += dau * (-Math.pow(x, k) / ms);
+            dau = -dau;
+        }
+        System.out.printf("Kết quả S(x,n) = %.5f%n", s);
+    }
 }
