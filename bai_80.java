c371ba70d5614393fc6f88e67978478d746069fd
tienhq <tienhq@ciaosoft.vn>
2026-03-19 10:20:19 +0700

    Update bai_80.java

--- bai_80.java
+++ bai_80.java
@@ -1,14 +1,18 @@
-Scanner sc = new Scanner(System.in);
+import java.util.Scanner;
 public class bai_80{
 //bài 70: Tính S(x,n) = 1 - 1/(1+2) + 1/(1+2+3) - ... + (-1)^n*1/(1+2+3+...+n)
-//nk = 2/(k*(k+1))
+//nk = (2*(-1)^(k+1))/(k*(k+1))
     public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập n: ");
         int n = sc.nextInt();
-        double s = sc.nextDouble();
-
+        double s = 0;
+        int dau = 1;
         for (int k = 1; k <= n; k++){
-            
+            double ms = (double) k * (k + 1)/2;
+            s += dau * (1.0/ms);
+            dau = -dau;
         }
+        System.out.printf("Kết quả S(n) = %.5f%n", s); 
     }
 }
